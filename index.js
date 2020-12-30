487014988311eabeb94cf9ac02cd29533252c4af
Vladimir Rodkin <mail@vovanr.com>
2020-12-30 11:09:56 +0300

    build(package): upgrade development dependencies; Update codebase
    Migrate from tabs to 2 spaces;
    
    BREAKING CHANGE: Drop support of Node.js < v10

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-nested-ternary */
-
 /**
  * Convert hour to chinese zodiac name
  *
@@ -11,23 +9,25 @@
  * //=> 'dragon'
  */
 function hourToChineseZodiac(hour) {
-	if ((!hour && hour !== 0) || hour > 24) {
-		return '';
-	}
+  if ((!hour && hour !== 0) || hour > 24) {
+    return '';
+  }
 
-	return hour > 22 ? 'rat' :
-		hour > 20 ? 'pig' :
-		hour > 18 ? 'dog' :
-		hour > 16 ? 'rooster' :
-		hour > 14 ? 'monkey' :
-		hour > 12 ? 'goat' :
-		hour > 10 ? 'horse' :
-		hour > 8 ? 'snake' :
-		hour > 6 ? 'dragon' :
-		hour > 4 ? 'rabbit' :
-		hour > 2 ? 'tiger' :
-		hour > 0 ? 'ox' :
-		'rat';
+  /* eslint-disable indent, no-multi-spaces, unicorn/no-nested-ternary */
+  return hour > 22 ? 'rat' :
+         hour > 20 ? 'pig' :
+         hour > 18 ? 'dog' :
+         hour > 16 ? 'rooster' :
+         hour > 14 ? 'monkey' :
+         hour > 12 ? 'goat' :
+         hour > 10 ? 'horse' :
+         hour >  8 ? 'snake' :
+         hour >  6 ? 'dragon' :
+         hour >  4 ? 'rabbit' :
+         hour >  2 ? 'tiger' :
+         hour >  0 ? 'ox' :
+                     'rat';
+  /* eslint-enable indent, no-multi-spaces, unicorn/no-nested-ternary */
 }
 
 module.exports = hourToChineseZodiac;
--- test.js
+++ test.js
@@ -1,45 +1,45 @@
-import test from 'ava';
-import fn from './';
+const test = require('ava');
+const fn = require('.');
 
 const r = {
-	rat: [0, 23],
-	ox: [1, 2],
-	tiger: [3, 4],
-	rabbit: [5, 6],
-	dragon: [7, 8],
-	snake: [9, 10],
-	horse: [11, 12],
-	goat: [13, 14],
-	monkey: [15, 16],
-	rooster: [17, 18],
-	dog: [19, 20],
-	pig: [21, 22]
+  rat: [0, 23],
+  ox: [1, 2],
+  tiger: [3, 4],
+  rabbit: [5, 6],
+  dragon: [7, 8],
+  snake: [9, 10],
+  horse: [11, 12],
+  goat: [13, 14],
+  monkey: [15, 16],
+  rooster: [17, 18],
+  dog: [19, 20],
+  pig: [21, 22]
 };
 
 test('is function', t => {
-	t.is(typeof fn, 'function');
+  t.is(typeof fn, 'function');
 });
 
 test('should convert number', t => {
-	Object.keys(r).forEach(zodiac => {
-		r[zodiac].forEach(hour => {
-			t.is(fn(hour), zodiac);
-		});
-	});
+  Object.keys(r).forEach(zodiac => {
+    r[zodiac].forEach(hour => {
+      t.is(fn(hour), zodiac);
+    });
+  });
 });
 
 test('should convert string', t => {
-	Object.keys(r).forEach(zodiac => {
-		r[zodiac].forEach(hour => {
-			t.is(fn(String(hour)), zodiac);
-		});
-	});
+  Object.keys(r).forEach(zodiac => {
+    r[zodiac].forEach(hour => {
+      t.is(fn(String(hour)), zodiac);
+    });
+  });
 });
 
 test('should not convert empty value', t => {
-	[undefined, null, ''].forEach(arg => t.is(fn(arg), ''));
+  [undefined, null, ''].forEach(arg => t.is(fn(arg), ''));
 });
 
 test('should not convert value > 24 hours', t => {
-	t.is(fn(25), '');
+  t.is(fn(25), '');
 });
