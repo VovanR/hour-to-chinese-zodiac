2b77d643a6b11e3275937e9d4594eb2e87c9689a
Vladimir Rodkin <mail@vovanr.com>
2020-12-30 11:48:05 +0300

    Updates

---
+++ index.js
@@ -0,0 +1,33 @@
+/**
+ * Convert hour to chinese zodiac name
+ *
+ * @param {number|string} hour
+ * @return {string} chinese zodiac name
+ *
+ * @example
+ * hourToChineseZodiac(8);
+ * //=> 'dragon'
+ */
+function hourToChineseZodiac(hour) {
+  if ((!hour && hour !== 0) || hour > 24) {
+    return '';
+  }
+
+  /* eslint-disable indent, no-multi-spaces, unicorn/no-nested-ternary */
+  return hour > 22 ? 'rat' :
+         hour > 20 ? 'pig' :
+         hour > 18 ? 'dog' :
+         hour > 16 ? 'rooster' :
+         hour > 14 ? 'monkey' :
+         hour > 12 ? 'goat' :
+         hour > 10 ? 'horse' :
+         hour >  8 ? 'snake' :
+         hour >  6 ? 'dragon' :
+         hour >  4 ? 'rabbit' :
+         hour >  2 ? 'tiger' :
+         hour >  0 ? 'ox' :
+                     'rat';
+  /* eslint-enable indent, no-multi-spaces, unicorn/no-nested-ternary */
+}
+
+module.exports = hourToChineseZodiac;
